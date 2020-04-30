41fe6bec8238692521c5b242cd856bc4546fac39
Julien <eoz.wild@gmail.com>
2020-04-30 09:45:35 +0200

    Reverted cities name in english

--- src/Model/Data.php
+++ src/Model/Data.php
@@ -13,16 +13,16 @@ class Data
     {
         return [
             'Paris' => 2968815,
-            'Londres' => 2643743,
+            'London' => 2643743,
             'Rio de Janeiro' => 3451190,
             'Santiago' => 3526709,
-            'New York' => 5128581,
+            'New York City' => 5128581,
             'Los-Angeles' => 5368361,
             'Tokyo' => 1850147,
-            'Pékin' => 2855016,
+            'Peking' => 2855016,
             'Bombay' => 2193111,
-            'Téhéran' => 112931,
-            'Alexandrie' => 361058,
+            'Tehran' => 112931,
+            'Alexandria' => 361058,
             'Nairobi' => 184742,
             'Pretoria' => 964137,
             'Abuja' => 2352778,
@@ -104,7 +104,7 @@ class Data
         return [
             'continents_europa.svg' => [
                 'Paris',
-                'Londres',
+                'London',
                 'Reykjavik'
             ],
             'continents_south_america.svg' => [
@@ -112,19 +112,19 @@ class Data
                 'Santiago'
             ],
             'continents_north_america.svg' => [
-                'New York',
+                'New York City',
                 'Los-Angeles',
                 'Anchorage',
                 'Vancouver'
             ],
             'continents_asia.svg' => [
                 'Tokyo',
-                'Pékin',
+                'Peking',
                 'Bombay',
-                'Tehéran'
+                'Tehran'
             ],
             'continents_africa.svg' => [
-                'Alexandrie',
+                'Alexandria',
                 'Nairobi',
                 'Pretoria',
                 'Abuja',
